c6a1d0c1900122d318adecaa300ce80daa850db0
Javier Urquizu <javierurquizu@gmail.com>
2020-03-19 23:04:48 -0700

    Added HTTP client w/ timeout & retry functionality to pull_data

--- pull_data.go
+++ pull_data.go
@@ -1,49 +1,94 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net/http"
-	"os"
+    "io"
+    "log"
+    "net/http"
+    "os"
+    "strings"
+    "time"
 )
 
-type DataDirector struct {
-	data_url, out_html string
+
+func get_timestamp(t time.Time) string {
+    datetime := strings.Split(t.String(), " ")
+    date := strings.Split(datetime[0], "-")
+    time := strings.Split(datetime[1], ":")
+
+    var YY, MM, DD, HH, mm string
+    YY = date[0]
+    MM = date[1]
+    DD = date[2]
+    HH = time[0]
+    mm = time[1]
+    return YY + MM + DD + "_" + HH + mm
 }
 
+func pull_data(input_url, output_file string) {
+    var (
+        err error
+        response *http.Response
+        retries int = 3
+        timeout time.Duration = 3 * time.Second
+    )
+
+    client := http.Client {
+        Timeout: time.Duration(timeout),
+    }
+
+    // Make request
+    for retries > 0 {
+        response, err = client.Get(input_url)
+
+        // Fail: Retry
+        if err != nil {
+            log.Println(err)
+            time.Sleep(timeout)
+            retries -= 1
+        } else {
+            // Success: Continue
+            break
+        }
+    }
+
+    if response != nil {
+        defer response.Body.Close()
+
+        // Create output file
+        outFile, err := os.Create(output_file)
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer outFile.Close()
+
+        // Copy data from HTTP response to file
+        _, err = io.Copy(outFile, response.Body)
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+
+}
+
+
+type DataDirector struct {
+    data_url, out_html string
+}
+
+
 func main() {
-	var out_dir = "data/"
+    var outdir = "test/"
 
-	var date, time string
-	date = "20200319"
-	time = "1626"
-	datetime := date + "_" + time
+    now := time.Now()
 
-	data_sites := make([]DataDirector, 2)
-	data_sites[0] = DataDirector{"https://www.worldometers.info/coronavirus/",
-								out_dir + "worldometers_" + datetime + ".html"}
-	data_sites[1] = DataDirector{"https://ncov2019.live/data", 
-								out_dir + "ncov2019_" + datetime + ".html"}
+    data_sites := make([]DataDirector, 2)
+    data_sites[0] = DataDirector{"https://www.worldometers.info/coronavirus/",
+                                 outdir + "worldometers_" + get_timestamp(now) + ".html"}
+    data_sites[1] = DataDirector{"https://ncov2019.live/data",
+                                 outdir + "ncov2019_" + get_timestamp(now) + ".html"}
 
-	for _, site := range data_sites {
-		// Make request
-		response, err := http.Get(site.data_url)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer response.Body.Close()
 
-		// Create output file
-		outFile, err := os.Create(site.out_html)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer outFile.Close()
-
-		// Copy data from HTTP response to file
-		_, err = io.Copy(outFile, response.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+    for _, site := range data_sites {
+        pull_data(site.data_url, site.out_html)
+    }
 }
